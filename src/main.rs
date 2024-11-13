5ff10ee2b7773aa5d4db1caf903f0aacf09cf122
Ivy <ivy.gifford04@gmail.com>
2024-11-13 14:48:34 -0500

    working nicely with icons

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 use bayou_fe::masto_types::timeline_item::Post;
 use gloo_net::http::Request;
 use leptos::{
-    component, create_resource, create_signal, html::div, view, For, HtmlElement, IntoView, ReadSignal, SignalGet, SignalSet, SignalUpdate
+    component, create_resource, html::div, view, For, IntoView, SignalGet
 };
-use serde::{Deserialize, Serialize};
+use leptos_lucide_icons::{Bookmark, MessageSquare, Repeat, Share2, Star};
 
 #[component]
 fn TimelinePost(post: Post) -> impl IntoView {
-    // todo!()
-    // let content = HtmlElement::inner_html(self, post.content);
     let content = div().inner_html(post.content);
 
     let display_name = match &post.account.display_name.is_empty() {
@@ -28,30 +26,25 @@ fn TimelinePost(post: Post) -> impl IntoView {
     view! {
         <div class="post">
         <hr />
-            <div class="user-link inline">
-                <a href={ format!("/@{}", post.account.acct) }>
+            <a href={ format!("/@{}", post.account.acct) } class="user-link inline no-decoration">
                     <img src={ post.account.avatar.clone() } class="timeline-pfp" />
-                </a>
-                <a href={ format!("/@{}", post.account.acct)} class="no-decoration">
+                <div href={ format!("/@{}", post.account.acct)} class="no-decoration">
                     <div class="inline">
                         <h3 class="no-margin">{ display_name }</h3>
                         {pronouns}
                     </div>
                     <p class="no-margin">{ format!("@{}", post.account.acct) }</p>
-                </a>
-            </div>
+                </div>
+            </a>
             {content}
 
-            // <p key={post.id.clone()}>
-            // { vnode }
-            // </p>
-            // <div class={classes!("status-actions")}>
-            //     <button><MessageSquare /></button>
-            //     <button><Repeat /></button>
-            //     <button><Star /></button>
-            //     <button><Bookmark /></button>
-            //     <button><Share2 /></button>
-            // </div>
+            <div class="status-actions">
+                <button><MessageSquare/></button>
+                <button><Repeat /></button>
+                <button><Star /></button>
+                <button><Bookmark /></button>
+                <button><Share2 /></button>
+            </div>
         <hr />
         </div>
     }
@@ -74,20 +67,7 @@ fn TimelineSegment(posts: Vec<Post>) -> impl IntoView {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-
     // see https://book.leptos.dev/async/10_resources.html
-    // let (posts, set_posts) = create_signal(0);
-
-    // let async_data = create_resource(
-    //     count,
-    //     // every time `count` changes, this will run
-    //     |value| async move {
-    //         logging::log!("loading data from API");
-    //         load_data(value).await
-    //     },
-    // );
-
     let timeline = create_resource(
         || (),
         |_| async move {
@@ -102,15 +82,6 @@ fn App() -> impl IntoView {
             fetched_posts
         },
     );
-
-    // let fetched_posts: Vec<Post> =
-    //                 Request::get("https://mastodon.social/api/v1/timelines/public")
-    //                     .send()
-    //                     .await
-    //                     .unwrap()
-    //                     .json()
-    //                     .await
-    //                     .unwrap();
 
     view! {
         <div class="timeline">
