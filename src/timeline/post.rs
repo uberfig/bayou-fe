1080dbae3817f214eba44286f5346f4f678c24d0
Ivy <ivy.gifford04@gmail.com>
2024-11-24 21:46:50 -0500

    improve rendering multiple content

--- src/timeline/post.rs
+++ src/timeline/post.rs
@@ -1,16 +1,10 @@
-use leptos::{component, html::div, view, IntoView};
+use leptos::{component, html::div, view, IntoView, View};
 use leptos_lucide_icons::{Bookmark, MessageSquare, Repeat, Share2, Star};
 
-use crate::masto_types::timeline_item::Status;
+use crate::masto_types::timeline_item::{MediaAttachment, Status};
 
-#[component]
-pub fn TimelinePost(post: Status) -> impl IntoView {
-    let source = post.clone();
-
-    let attachments = match post.media_attachments {
-        Some(attachments) => {
-            // Some(
-            attachments.into_iter()
+pub fn generate_attachments(attachments: Vec<MediaAttachment>) -> View {
+    let mut attachments = attachments.into_iter()
             .map(|attachment| {
                 match attachment.type_field {
                     crate::masto_types::timeline_item::MediaType::Unknown => view! {<a href={attachment.url.to_string()}>{attachment.url.to_string()}</a>}.into_view(),
@@ -27,17 +21,101 @@ pub fn TimelinePost(post: Status) -> impl IntoView {
                     }.into_view(),
                 }
             })
-            .collect::<Vec<_>>()
+            .collect::<Vec<_>>();
+    
+    match attachments.len() {
+        0 => view! {}.into_view(),
+        1 => view! {
+            <div class="attachment-container">
+                {attachments}
+            </div>
+        }.into_view(),
+        2 => {
+            let first = attachments.remove(0);
+            let second = attachments.remove(0);
+            view! {
+                <div class="attachment-container">
+                    <div class="multiple-attachment">
+                        <div class="w50">
+                            {first}
+                        </div>
+                        <div class="w50">
+                            {second}
+                        </div>
+                    </div>
+                </div>
+            }.into_view()
         }
-        None => Vec::new(),
+        3 => {
+            let first = attachments.remove(0);
+            view! {
+                <div class="attachment-container">
+                    <div class="multiple-attachment">
+                        <div class="primary w75">
+                            {first}
+                        </div>
+                        <div class="secondary w25">
+                            {attachments}
+                        </div>
+                    </div>
+                </div>
+            }.into_view()
+        },
+        4 =>  {
+
+            let first = attachments.remove(0);
+            let second = attachments.remove(0);
+            let third = attachments.remove(0);
+            let fourth = attachments.remove(0);
+            view! {
+                <div class="attachment-container">
+                    <div class="multiple-attachment">
+                        <div class="secondary w50">
+                            {first}
+                            {second}
+                        </div>
+                        <div class="secondary w50">
+                            {third}
+                            {fourth}
+                        </div>
+                    </div>
+                </div>
+            }.into_view()
+        
+        }
+        _ => {
+            let first = attachments.remove(0);
+            view! {
+                <div class="attachment-container">
+                    <div class="multiple-attachment">
+                        <div class="primary w75">
+                            {first}
+                        </div>
+                        <div class="secondary w25">
+                            {attachments}
+                        </div>
+                    </div>
+                </div>
+            }.into_view()
+        }
+    }
+}
+
+#[component]
+pub fn TimelinePost(post: Status) -> impl IntoView {
+    let source = post.clone();
+
+    let attachments = match post.media_attachments {
+        Some(attachments) => {
+            generate_attachments(attachments)
+        }
+        None => view! {}.into_view(),
     };
 
     let content = div().inner_html(post.content).attr("class", "post-body");
     let content = view! {
         {content}
-        <div class="attachment-container">
-            {attachments}
-        </div>
+        {attachments}
     };
     let content = match post.sensitive {
         true => match post.spoiler_text.is_empty() {
